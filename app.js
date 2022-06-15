0a688c10d15b5ea447c5577650c6236026bae66e
Swarna Sarker <swarnaprony81@gmail.com>
2022-06-15 15:41:00 +0200

    Create unordered list from user input

--- app.js
+++ app.js
@@ -1,46 +1,38 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const req = require('express/lib/request');
 
 const app = express();
 
+var items = ["Shop", "Cook", "Eat"];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res) {
     var today = new Date();
-    
-    var currentDay = today.getDay();
-    var day = "";
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday"
-            break;
-        case 1:
-            day = "Monday"
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Wednesday"
-            break;
-        case 4:
-            day = "Thursday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;
-        default:
-            console.log("Error: current day is equal to: " + currentDay);
-    }
+    var today = new Date();
 
-    res.render("list", { kindOfDay: day });
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleString("en-us", options);
+
+    res.render("list", { kindOfDay: day, newListItem: items});
 });
 
 
+app.post("/", function(req, res) {
+    var item = req.body.newItem;
+    items.push(item)
+    res.redirect("/");
+    
+})
 
 
 app.listen(3000, function () {
