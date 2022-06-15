4de097e0303d822d4248786e21d796a4e63640ac
Swarna Sarker <swarnaprony81@gmail.com>
2022-06-15 17:54:16 +0200

    Create Separate Work list for work day and about page

--- app.js
+++ app.js
@@ -4,35 +4,50 @@ const req = require('express/lib/request');
 
 const app = express();
 
-var items = ["Shop", "Cook", "Eat"];
+let items = ["Shop", "Cook", "Eat"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    var today = new Date();
 
-    var today = new Date();
+    let today = new Date();
 
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleString("en-us", options);
+    let day = today.toLocaleString("en-us", options);
 
-    res.render("list", { kindOfDay: day, newListItem: items});
+    res.render("list", { listTitle: day, newListItems: items});
 });
 
 
 app.post("/", function(req, res) {
-    var item = req.body.newItem;
-    items.push(item)
-    res.redirect("/");
-    
+
+    let item = req.body.newItem;
+    console.log(req.body.list)
+
+    if (req.body.list === "Work List") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item) 
+        res.redirect("/");
+    }
+        
+});
+
+app.get("/work", function(req, res) {
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
 })
+
+
 
 
 app.listen(3000, function () {
